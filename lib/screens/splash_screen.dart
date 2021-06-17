b68360e8e69dcc9f0040a3016edbaddd57c6a0f1
rishavk1102 <rishavk1102@gmail.com>
2021-06-17 15:40:19 +0530

    Added loading screen animation

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,21 +1,71 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool showLoadingSellers = false;
+  dynamic _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer(
+        const Duration(seconds: 3),
+        () => {
+              showLoadingSellers = true,
+              setState(() {}),
+            });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        color: Color(0xffE6F3EC),
-        child: Text(
-          'Better Buys',
-          style: GoogleFonts.pacifico(
-            color: Color(0xff4E8489),
-            fontSize: 27,
-          ),
+        color: const Color(0xffE6F3EC),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Better Buys',
+              style: GoogleFonts.pacifico(
+                color: const Color(0xff4E8489),
+                fontSize: 27,
+              ),
+            ),
+            if (showLoadingSellers)
+              const SizedBox(
+                height: 20.0,
+                width: 20.0,
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xff4E8489)),
+                  strokeWidth: 1.5,
+                ),
+              ),
+            if (showLoadingSellers)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  'Loading Sellers',
+                  style: GoogleFonts.poppins(),
+                ),
+              ),
+          ],
         ),
       ),
     );
