270632455c0f86b4943d0ce3c91a764208785bb7
rishavk1102 <rishavk1102@gmail.com>
2021-07-01 13:26:30 +0530

    Sellers ui complete

--- lib/widgets/seller_item.dart
+++ lib/widgets/seller_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../models/seller.dart';
 
@@ -23,6 +24,65 @@ class SellerItem extends StatelessWidget {
       decoration: BoxDecoration(
         color: backgroundColor,
         borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  top: 30.0,
+                ),
+                child: Text(
+                  seller.name!,
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                width: 164.0,
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  top: 5.0,
+                ),
+                child: Text(
+                  seller.address!,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 13.0,
+                  ),
+                ),
+              ),
+              Container(
+                width: 164.0,
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  top: 5.0,
+                ),
+                child: Text(
+                  'No Rating',
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.poppins(
+                    color: Colors.grey,
+                    fontSize: 13.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Image.network(
+                "http://whoisrishav.com/pk/better-buys/assets/${seller.image!}"),
+          ),
+        ],
       ),
     );
   }
